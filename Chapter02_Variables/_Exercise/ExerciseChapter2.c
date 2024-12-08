36e569cf98f1a07a6a146912ad21808204812f73
catnessity <xlisax94@gmail.com>
2024-12-08 15:03:27 +0000

    chore: exercise

--- Chapter02_Variables/_Exercise/ExerciseChapter2.c
+++ Chapter02_Variables/_Exercise/ExerciseChapter2.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main()
 {
+    uint64_t input;
+    printf("Enter age: ");
+    scanf("%lu", &input);
+    printf("Years:      %lu\n", input);
 
+    input *= 365;
+    printf("Days:       %lu\n", input);
+
+    input *= 24;
+    printf("Hours:      %lu\n", input);
+
+    input *= 60;
+    printf("Minutes:    %lu\n", input);
+
+    input *= 60;
+    printf("Sedonds:    %lu\n", input);
 
     return 0;
 }
